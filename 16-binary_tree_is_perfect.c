eac0632876b3c6d6e9c02f3ceccdd5e15d7ab8bb
Maniok19 <mano19029@hotmail.fr>
2024-12-09 17:51:40 +0100

    refactor: improve binary tree height and perfection checks

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,56 +1,61 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
+#include <stdio.h>
 
-size_t find_height(const binary_tree_t *tree)
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	left_height = find_height(tree->left);
-	right_height = find_height(tree->right);
 
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is full, 0 otherwise
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    if (tree->left && tree->right)
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-    return (0);
+	return (0);
 }
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t height, node, i;
+	size_t height_left, height_right;
 
+	if (tree == NULL)
+		return (0);
 
-    if (tree == NULL)
-        return (0);
-height = find_height(tree);
-node = 1;
-for (i = 0; i < height; i++)
-	node = node * 2;
-node--;
-    /*if (tree->right && tree->left)
-	return (1);
-	if (binary_tree_is_full(tree) != 0 && tree->right && tree->left)
-		return (1);*/
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
 
-	return (binary_tree_is_full(tree) && binary_tree_size(tree) == node);
-}
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-if (tree == NULL)
-	return (0);
-return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	/* Check if the tree is full and both subtrees have the same height */
+	return (binary_tree_is_full(tree) && height_left == height_right);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,26 +1,28 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * binary_tree_insert_right - inserts a node as the right-child of another node
- * @parent: pointer to the node to insert the right-child in
- * @value: value to store in the new node
- * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 		return (NULL);
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->right = parent->right;
-	new_node->left = NULL;
-	if (parent->right)
+
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
 		parent->right->parent = new_node;
+	}
 	parent->right = new_node;
+
 	return (new_node);
 }
