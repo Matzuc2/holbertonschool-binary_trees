07aa3fd342959ed77ec41127450f89463dfa059f
Maniok19 <mano19029@hotmail.fr>
2024-12-09 15:01:35 +0100

    feat: implement binary_tree_size and binary_tree_leaves functions with main test files

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,3 +6,13 @@
  * Return: Size of the tree, or 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (!tree)
+		return (0);
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	size++;
+	return (size);
+}
