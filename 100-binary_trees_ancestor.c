3b568a168e777b7d84c3634ed21b487103c4161e
Maniok19 <mano19029@hotmail.fr>
2024-12-12 10:08:33 +0100

    feat: enhance binary_trees_ancestor function to handle edge cases and improve ancestor retrieval logic

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,7 +6,21 @@
  * @second: Pointer to the second node
  * Return: Pointer to the lowest common ancestor node of the two given nodes
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
 {
+	binary_tree_t *ancestor;
 
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+	if (first->parent == second)
+		return ((binary_tree_t *)second);
+	if (second->parent == first)
+		return ((binary_tree_t *)first);
+	if (first->parent == second->parent)
+		return (first->parent);
+	ancestor = binary_trees_ancestor(first->parent, second->parent);
+	return (ancestor);
 }
