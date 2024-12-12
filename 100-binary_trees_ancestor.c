d7f85d362475405116dbd48e854ffefecc4c8712
Maniok19 <mano19029@hotmail.fr>
2024-12-12 09:59:09 +0100

    feat: add function to find lowest common ancestor of two nodes

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ * Return: Pointer to the lowest common ancestor node of the two given nodes
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+
+}
